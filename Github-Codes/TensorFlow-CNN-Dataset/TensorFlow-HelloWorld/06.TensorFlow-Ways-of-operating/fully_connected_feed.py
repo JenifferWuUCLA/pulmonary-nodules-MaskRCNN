9fd8c955af3fa670c355cd9180dbd9ecfc66a79c
root <yywu@szucla.org>
2018-02-07 16:23:02 +0800

    Reformat the codes.

--- Github-Codes/TensorFlow-CNN-Dataset/TensorFlow-HelloWorld/06.TensorFlow-Ways-of-operating/fully_connected_feed.py
+++ Github-Codes/TensorFlow-CNN-Dataset/TensorFlow-HelloWorld/06.TensorFlow-Ways-of-operating/fully_connected_feed.py
@@ -21,20 +21,20 @@ FLAGS = None
 
 
 def placeholder_inputs(batch_size):
-  images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
-                                                         mnist.IMAGE_PIXELS))
-  labels_placeholder = tf.placeholder(tf.int32, shape=(batch_size))
-  return images_placeholder, labels_placeholder
+    images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
+                                                           mnist.IMAGE_PIXELS))
+    labels_placeholder = tf.placeholder(tf.int32, shape=(batch_size))
+    return images_placeholder, labels_placeholder
 
 
 def fill_feed_dict(data_set, images_pl, labels_pl):
-  images_feed, labels_feed = data_set.next_batch(FLAGS.batch_size,
-                                                 FLAGS.fake_data)
-  feed_dict = {
-      images_pl: images_feed,
-      labels_pl: labels_feed,
-  }
-  return feed_dict
+    images_feed, labels_feed = data_set.next_batch(FLAGS.batch_size,
+                                                   FLAGS.fake_data)
+    feed_dict = {
+        images_pl: images_feed,
+        labels_pl: labels_feed,
+    }
+    return feed_dict
 
 
 def do_eval(sess,
@@ -42,163 +42,151 @@ def do_eval(sess,
             images_placeholder,
             labels_placeholder,
             data_set):
-
-  true_count = 0  # Counts the number of correct predictions.
-  steps_per_epoch = data_set.num_examples // FLAGS.batch_size
-  num_examples = steps_per_epoch * FLAGS.batch_size
-  for step in xrange(steps_per_epoch):
-    feed_dict = fill_feed_dict(data_set,
-                               images_placeholder,
-                               labels_placeholder)
-    true_count += sess.run(eval_correct, feed_dict=feed_dict)
-  precision = float(true_count) / num_examples
-  print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
-        (num_examples, true_count, precision))
+    true_count = 0  # Counts the number of correct predictions.
+    steps_per_epoch = data_set.num_examples // FLAGS.batch_size
+    num_examples = steps_per_epoch * FLAGS.batch_size
+    for step in xrange(steps_per_epoch):
+        feed_dict = fill_feed_dict(data_set,
+                                   images_placeholder,
+                                   labels_placeholder)
+        true_count += sess.run(eval_correct, feed_dict=feed_dict)
+    precision = float(true_count) / num_examples
+    print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
+          (num_examples, true_count, precision))
 
 
 def run_training():
-  """Train MNIST for a number of steps."""
-  data_sets = input_data.read_data_sets(FLAGS.input_data_dir, FLAGS.fake_data)
+    """Train MNIST for a number of steps."""
+    data_sets = input_data.read_data_sets(FLAGS.input_data_dir, FLAGS.fake_data)
 
-  with tf.Graph().as_default():
-    images_placeholder, labels_placeholder = placeholder_inputs(
-        FLAGS.batch_size)
+    with tf.Graph().as_default():
+        images_placeholder, labels_placeholder = placeholder_inputs(
+            FLAGS.batch_size)
 
-    logits = mnist.inference(images_placeholder,
-                             FLAGS.hidden1,
-                             FLAGS.hidden2)
+        logits = mnist.inference(images_placeholder,
+                                 FLAGS.hidden1,
+                                 FLAGS.hidden2)
 
-    loss = mnist.loss(logits, labels_placeholder)
+        loss = mnist.loss(logits, labels_placeholder)
 
-    train_op = mnist.training(loss, FLAGS.learning_rate)
+        train_op = mnist.training(loss, FLAGS.learning_rate)
 
+        eval_correct = mnist.evaluation(logits, labels_placeholder)
 
-    eval_correct = mnist.evaluation(logits, labels_placeholder)
+        summary = tf.summary.merge_all()
 
+        init = tf.global_variables_initializer()
 
-    summary = tf.summary.merge_all()
+        saver = tf.train.Saver()
 
+        sess = tf.Session()
 
-    init = tf.global_variables_initializer()
+        summary_writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
 
+        sess.run(init)
 
-    saver = tf.train.Saver()
+        for step in xrange(FLAGS.max_steps):
+            start_time = time.time()
 
-    sess = tf.Session()
+            feed_dict = fill_feed_dict(data_sets.train,
+                                       images_placeholder,
+                                       labels_placeholder)
 
+            _, loss_value = sess.run([train_op, loss],
+                                     feed_dict=feed_dict)
 
-    summary_writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
+            duration = time.time() - start_time
 
+            if step % 100 == 0:
+                print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
+                # Update the events file.
+                summary_str = sess.run(summary, feed_dict=feed_dict)
+                summary_writer.add_summary(summary_str, step)
+                summary_writer.flush()
 
-    sess.run(init)
+            if (step + 1) % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+                checkpoint_file = os.path.join(FLAGS.log_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_file, global_step=step)
 
+                print('Training Data Eval:')
+                do_eval(sess,
+                        eval_correct,
+                        images_placeholder,
+                        labels_placeholder,
+                        data_sets.train)
 
-    for step in xrange(FLAGS.max_steps):
-      start_time = time.time()
+                print('Validation Data Eval:')
+                do_eval(sess,
+                        eval_correct,
+                        images_placeholder,
+                        labels_placeholder,
+                        data_sets.validation)
 
-
-      feed_dict = fill_feed_dict(data_sets.train,
-                                 images_placeholder,
-                                 labels_placeholder)
-
-
-      _, loss_value = sess.run([train_op, loss],
-                               feed_dict=feed_dict)
-
-      duration = time.time() - start_time
-
-
-      if step % 100 == 0:
-
-        print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
-        # Update the events file.
-        summary_str = sess.run(summary, feed_dict=feed_dict)
-        summary_writer.add_summary(summary_str, step)
-        summary_writer.flush()
-
-      if (step + 1) % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-        checkpoint_file = os.path.join(FLAGS.log_dir, 'model.ckpt')
-        saver.save(sess, checkpoint_file, global_step=step)
-
-        print('Training Data Eval:')
-        do_eval(sess,
-                eval_correct,
-                images_placeholder,
-                labels_placeholder,
-                data_sets.train)
-
-        print('Validation Data Eval:')
-        do_eval(sess,
-                eval_correct,
-                images_placeholder,
-                labels_placeholder,
-                data_sets.validation)
-
-        print('Test Data Eval:')
-        do_eval(sess,
-                eval_correct,
-                images_placeholder,
-                labels_placeholder,
-                data_sets.test)
+                print('Test Data Eval:')
+                do_eval(sess,
+                        eval_correct,
+                        images_placeholder,
+                        labels_placeholder,
+                        data_sets.test)
 
 
 def main(_):
-  if tf.gfile.Exists(FLAGS.log_dir):
-    tf.gfile.DeleteRecursively(FLAGS.log_dir)
-  tf.gfile.MakeDirs(FLAGS.log_dir)
-  run_training()
+    if tf.gfile.Exists(FLAGS.log_dir):
+        tf.gfile.DeleteRecursively(FLAGS.log_dir)
+    tf.gfile.MakeDirs(FLAGS.log_dir)
+    run_training()
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--learning_rate',
-      type=float,
-      default=0.01,
-      help='Initial learning rate.'
-  )
-  parser.add_argument(
-      '--max_steps',
-      type=int,
-      default=2000,
-      help='Number of steps to run trainer.'
-  )
-  parser.add_argument(
-      '--hidden1',
-      type=int,
-      default=128,
-      help='Number of units in hidden layer 1.'
-  )
-  parser.add_argument(
-      '--hidden2',
-      type=int,
-      default=32,
-      help='Number of units in hidden layer 2.'
-  )
-  parser.add_argument(
-      '--batch_size',
-      type=int,
-      default=100,
-      help='Batch size.  Must divide evenly into the dataset sizes.'
-  )
-  parser.add_argument(
-      '--input_data_dir',
-      type=str,
-      default='/tmp/tensorflow/mnist/input_data',
-      help='Directory to put the input data.'
-  )
-  parser.add_argument(
-      '--log_dir',
-      type=str,
-      default='/tmp/tensorflow/mnist/logs/fully_connected_feed',
-      help='Directory to put the log data.'
-  )
-  parser.add_argument(
-      '--fake_data',
-      default=False,
-      help='If true, uses fake data for unit testing.',
-      action='store_true'
-  )
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--learning_rate',
+        type=float,
+        default=0.01,
+        help='Initial learning rate.'
+    )
+    parser.add_argument(
+        '--max_steps',
+        type=int,
+        default=2000,
+        help='Number of steps to run trainer.'
+    )
+    parser.add_argument(
+        '--hidden1',
+        type=int,
+        default=128,
+        help='Number of units in hidden layer 1.'
+    )
+    parser.add_argument(
+        '--hidden2',
+        type=int,
+        default=32,
+        help='Number of units in hidden layer 2.'
+    )
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        default=100,
+        help='Batch size.  Must divide evenly into the dataset sizes.'
+    )
+    parser.add_argument(
+        '--input_data_dir',
+        type=str,
+        default='/tmp/tensorflow/mnist/input_data',
+        help='Directory to put the input data.'
+    )
+    parser.add_argument(
+        '--log_dir',
+        type=str,
+        default='/tmp/tensorflow/mnist/logs/fully_connected_feed',
+        help='Directory to put the log data.'
+    )
+    parser.add_argument(
+        '--fake_data',
+        default=False,
+        help='If true, uses fake data for unit testing.',
+        action='store_true'
+    )
 
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- Github-Codes/TensorFlow-CNN-Dataset/TensorFlow-HelloWorld/06.TensorFlow-Ways-of-operating/mnist.py
+++ Github-Codes/TensorFlow-CNN-Dataset/TensorFlow-HelloWorld/06.TensorFlow-Ways-of-operating/mnist.py
@@ -7,67 +7,61 @@ import math
 
 import tensorflow as tf
 
-
 NUM_CLASSES = 10
-
 
 IMAGE_SIZE = 28
 IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
 
 def inference(images, hidden1_units, hidden2_units):
-  # Hidden 1
-  with tf.name_scope('hidden1'):
-    weights = tf.Variable(
-        tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
-                            stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([hidden1_units]),
-                         name='biases')
-    hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
-  # Hidden 2
-  with tf.name_scope('hidden2'):
-    weights = tf.Variable(
-        tf.truncated_normal([hidden1_units, hidden2_units],
-                            stddev=1.0 / math.sqrt(float(hidden1_units))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([hidden2_units]),
-                         name='biases')
-    hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
-  # Linear
-  with tf.name_scope('softmax_linear'):
-    weights = tf.Variable(
-        tf.truncated_normal([hidden2_units, NUM_CLASSES],
-                            stddev=1.0 / math.sqrt(float(hidden2_units))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([NUM_CLASSES]),
-                         name='biases')
-    logits = tf.matmul(hidden2, weights) + biases
-  return logits
+    # Hidden 1
+    with tf.name_scope('hidden1'):
+        weights = tf.Variable(
+            tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
+                                stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
+            name='weights')
+        biases = tf.Variable(tf.zeros([hidden1_units]),
+                             name='biases')
+        hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
+    # Hidden 2
+    with tf.name_scope('hidden2'):
+        weights = tf.Variable(
+            tf.truncated_normal([hidden1_units, hidden2_units],
+                                stddev=1.0 / math.sqrt(float(hidden1_units))),
+            name='weights')
+        biases = tf.Variable(tf.zeros([hidden2_units]),
+                             name='biases')
+        hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
+    # Linear
+    with tf.name_scope('softmax_linear'):
+        weights = tf.Variable(
+            tf.truncated_normal([hidden2_units, NUM_CLASSES],
+                                stddev=1.0 / math.sqrt(float(hidden2_units))),
+            name='weights')
+        biases = tf.Variable(tf.zeros([NUM_CLASSES]),
+                             name='biases')
+        logits = tf.matmul(hidden2, weights) + biases
+    return logits
 
 
 def loss(logits, labels):
-
-  labels = tf.to_int64(labels)
-  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-      labels=labels, logits=logits, name='xentropy')
-  return tf.reduce_mean(cross_entropy, name='xentropy_mean')
+    labels = tf.to_int64(labels)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        labels=labels, logits=logits, name='xentropy')
+    return tf.reduce_mean(cross_entropy, name='xentropy_mean')
 
 
 def training(loss, learning_rate):
+    tf.summary.scalar('loss', loss)
 
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
 
-  tf.summary.scalar('loss', loss)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
 
-  optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-
-  global_step = tf.Variable(0, name='global_step', trainable=False)
-
-  train_op = optimizer.minimize(loss, global_step=global_step)
-  return train_op
+    train_op = optimizer.minimize(loss, global_step=global_step)
+    return train_op
 
 
 def evaluation(logits, labels):
-
-  correct = tf.nn.in_top_k(logits, labels, 1)
-  return tf.reduce_sum(tf.cast(correct, tf.int32))
+    correct = tf.nn.in_top_k(logits, labels, 1)
+    return tf.reduce_sum(tf.cast(correct, tf.int32))
