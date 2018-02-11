d9c5cea239d10713745d1735ab4da05775886042
root <yywu@szucla.org>
2018-02-11 15:15:37 +0800

    Reformat the codes.

--- pulmonary-nodules-Demos/classical-CNN/train_model_using_own_dataset/01/15-2.py
+++ pulmonary-nodules-Demos/classical-CNN/train_model_using_own_dataset/01/15-2.py
@@ -10,8 +10,8 @@ X_train, y_train = reader2.get_file("c:\\cat_and_dog_r")
 
 image_batch, label_batch = reader2.get_batch(X_train, y_train, 227, 227, 200, 2048)
 
-def batch_norm(inputs, is_training,is_conv_out=True,decay = 0.999):
 
+def batch_norm(inputs, is_training, is_conv_out=True, decay=0.999):
     scale = tf.Variable(tf.ones([inputs.get_shape()[-1]]))
     beta = tf.Variable(tf.zeros([inputs.get_shape()[-1]]))
     pop_mean = tf.Variable(tf.zeros([inputs.get_shape()[-1]]), trainable=False)
@@ -19,19 +19,20 @@ def batch_norm(inputs, is_training,is_conv_out=True,decay = 0.999):
 
     if is_training:
         if is_conv_out:
-            batch_mean, batch_var = tf.nn.moments(inputs,[0,1,2])
+            batch_mean, batch_var = tf.nn.moments(inputs, [0, 1, 2])
         else:
-            batch_mean, batch_var = tf.nn.moments(inputs,[0])
+            batch_mean, batch_var = tf.nn.moments(inputs, [0])
 
         train_mean = tf.assign(pop_mean, pop_mean * decay + batch_mean * (1 - decay))
         train_var = tf.assign(pop_var, pop_var * decay + batch_var * (1 - decay))
 
         with tf.control_dependencies([train_mean, train_var]):
             return tf.nn.batch_normalization(inputs,
-                batch_mean, batch_var, beta, scale, 0.001)
+                                             batch_mean, batch_var, beta, scale, 0.001)
     else:
         return tf.nn.batch_normalization(inputs,
-            pop_mean, pop_var, beta, scale, 0.001)
+                                         pop_mean, pop_var, beta, scale, 0.001)
+
 
 with tf.device('/cpu:0'):
     # 模型参数
@@ -73,7 +74,7 @@ with tf.device('/cpu:0'):
     # 卷积层 1
     conv1 = tf.nn.conv2d(x_image, W_conv['conv1'], strides=[1, 4, 4, 1], padding='VALID')
     conv1 = tf.nn.bias_add(conv1, b_conv['conv1'])
-    conv1 = batch_norm(conv1,True)
+    conv1 = batch_norm(conv1, True)
     conv1 = tf.nn.relu(conv1)
     # 池化层 1
     pool1 = tf.nn.avg_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
@@ -82,7 +83,7 @@ with tf.device('/cpu:0'):
     # 卷积层 2
     conv2 = tf.nn.conv2d(pool1, W_conv['conv2'], strides=[1, 1, 1, 1], padding='SAME')
     conv2 = tf.nn.bias_add(conv2, b_conv['conv2'])
-    conv2 = batch_norm(conv2,True)
+    conv2 = batch_norm(conv2, True)
     conv2 = tf.nn.relu(conv2)
     # 池化层 2
     pool2 = tf.nn.avg_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
@@ -90,31 +91,31 @@ with tf.device('/cpu:0'):
     # 卷积层3
     conv3 = tf.nn.conv2d(pool2, W_conv['conv3'], strides=[1, 1, 1, 1], padding='SAME')
     conv3 = tf.nn.bias_add(conv3, b_conv['conv3'])
-    conv3 = batch_norm(conv3,True)
+    conv3 = batch_norm(conv3, True)
     conv3 = tf.nn.relu(conv3)
 
     # 卷积层4
     conv4 = tf.nn.conv2d(conv3, W_conv['conv4'], strides=[1, 1, 1, 1], padding='SAME')
     conv4 = tf.nn.bias_add(conv4, b_conv['conv4'])
-    conv4 = batch_norm(conv4,True)
+    conv4 = batch_norm(conv4, True)
     conv4 = tf.nn.relu(conv4)
 
     # 卷积层5
     conv5 = tf.nn.conv2d(conv4, W_conv['conv5'], strides=[1, 1, 1, 1], padding='SAME')
     conv5 = tf.nn.bias_add(conv5, b_conv['conv5'])
-    conv5 = batch_norm(conv5,True)
+    conv5 = batch_norm(conv5, True)
     conv5 = tf.nn.relu(conv2)
 
     # 池化层5
     pool5 = tf.nn.avg_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
     reshape = tf.reshape(pool5, [-1, 13 * 13 * 256])
     fc1 = tf.add(tf.matmul(reshape, W_conv['fc1']), b_conv['fc1'])
-    fc1 = batch_norm(fc1,True,False)
+    fc1 = batch_norm(fc1, True, False)
     fc1 = tf.nn.relu(fc1)
 
     # 全连接层 2
     fc2 = tf.add(tf.matmul(fc1, W_conv['fc2']), b_conv['fc2'])
-    fc2 = batch_norm(fc2,True,False)
+    fc2 = batch_norm(fc2, True, False)
     fc2 = tf.nn.relu(fc2)
     fc3 = tf.add(tf.matmul(fc2, W_conv['fc3']), b_conv['fc3'])
 
@@ -127,6 +128,7 @@ with tf.device('/cpu:0'):
 
     init = tf.global_variables_initializer()
 
+
 def onehot(labels):
     '''one-hot 编码'''
     n_sample = len(labels)
@@ -135,7 +137,10 @@ def onehot(labels):
     onehot_labels[np.arange(n_sample), labels] = 1
     return onehot_labels
 
+
 save_model = ".//model//AlexNetModel.ckpt"
+
+
 def train(opech):
     with tf.Session() as sess:
         sess.run(init)
@@ -165,7 +170,7 @@ def train(opech):
             start_time = end_time
             print("---------------%d onpech is finished-------------------" % i)
         print("Optimization Finished!")
-#        checkpoint_path = os.path.join(".//model", 'model.ckpt')  # 输出模型的地方
+        #        checkpoint_path = os.path.join(".//model", 'model.ckpt')  # 输出模型的地方
         saver.save(sess, save_model)
         print("Model Save Finished!")
 
